0f4f99851e06a94be3a3a258074f4da1bf457774
Emil Augustynowicz <emilaugustynowicz@gmail.com>
2024-02-27 21:36:20 +0100

    Added fonts and icons

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,23 @@
+import "./Main.scss";
+
 const MainSection = () => {
   return (
     <>
       <main>
         <section>
-          <h1>YYYY</h1>
+          <h1>Emil Augustynowicz</h1>
+          <h1>- Web Developer</h1>
+          <h1>- Photographer</h1>
         </section>
         <section>
-          <h2>YYYY</h2>
+          <h2>Frontend Developer</h2>
+          <h2>Web Developer</h2>
+          <h2>Photographer</h2>
+        </section>
+        <section>
+          <h2>Frontend Developer</h2>
+          <h2>Web Developer</h2>
+          <h2>Photographer</h2>
         </section>
       </main>
     </>
