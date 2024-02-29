27d7c4a811d2e41711c01391d3972b1e609c3db4
Emil Augustynowicz <emilaugustynowicz@gmail.com>
2024-02-29 09:59:52 +0100

    Section styled component added

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,24 +1,24 @@
-import "./Main.scss";
+import Section from "./Section";
 
 const MainSection = () => {
   return (
     <>
       <main>
-        <section>
+        <Section>
           <h1>Emil Augustynowicz</h1>
           <h1>- Web Developer</h1>
           <h1>- Photographer</h1>
-        </section>
-        <section>
+        </Section>
+        <Section>
           <h2>Frontend Developer</h2>
           <h2>Web Developer</h2>
           <h2>Photographer</h2>
-        </section>
-        <section>
+        </Section>
+        <Section>
           <h2>Frontend Developer</h2>
           <h2>Web Developer</h2>
           <h2>Photographer</h2>
-        </section>
+        </Section>
       </main>
     </>
   );
---
+++ src/components/Section.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Section = styled.section`
+  min-height: 100dvh;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-content: center;
+  justify-content: center;
+  align-items: start;
+`;
+
+export default Section;
