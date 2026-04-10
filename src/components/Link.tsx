a16d5816ece7842526fd03e3c15e42f1e5919cf0
Zdechlak <emilaugustynowicz@gmail.com>
2026-04-10 16:01:20 +0200

    added scroll animation by AI

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,5 +1,41 @@
 import styled from "styled-components";
+import { gsap } from "gsap";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 
-const Link = styled.a``;
+gsap.registerPlugin(ScrollToPlugin);
+
+interface LinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+  href: string;
+  children: React.ReactNode;
+}
+
+const StyledLink = styled.a``;
+
+const Link: React.FC<LinkProps> = ({ href, children, ...props }) => {
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    if (href.startsWith("#")) {
+      e.preventDefault();
+      const targetId = href.substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        gsap.to(window, {
+          duration: 1,
+          scrollTo: {
+            y: targetElement,
+            offsetY: 60,
+          },
+          ease: "power2.inOut",
+        });
+      }
+    }
+  };
+
+  return (
+    <StyledLink href={href} onClick={handleClick} {...props}>
+      {children}
+    </StyledLink>
+  );
+};
 
 export default Link;
